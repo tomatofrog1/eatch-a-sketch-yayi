8def4f6306eb5eaf35b737ee3c65646e2260c4b4
odin_yayi <alcosharley21@gmail.com>
2023-10-04 21:21:11 +0800

    did the changegrid on the ui

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-const container = document.querySelector('.container');
-let row;
+const grid = document.querySelector('.grid');
+
 
 
 function createGrid(rnc) {
+    let colummn;
     const div = document.createElement('div')
     div.classList.add('div')
 
     for (let i = 0; i < rnc; i ++) {
-        container.appendChild(div.cloneNode(true))
+        grid.appendChild(div.cloneNode(true))
         
         
     }
-    row = document.querySelectorAll('.div')
+    colummn = document.querySelectorAll('.div')
 
     for (let i = 0; i < rnc; i++) {
         for (let i = 0; i < rnc; i++) {
-            row[i].appendChild(div.cloneNode(true))
+            colummn[i].appendChild(div.cloneNode(true))
             
         }
         
     }
 }
 
-window.onload = createGrid(16)
+
 
 //hover effect 
-const allDiv = document.querySelectorAll('.container>div>div')
-for (let i = 0; i < allDiv.length; i++) {
+function hoverEffect() {
+    const allDiv = document.querySelectorAll('.grid>div>div')
+    for (let i = 0; i < allDiv.length; i++) {
     allDiv[i].addEventListener('mouseover', function handleMouseOver() {
         allDiv[i].style.backgroundColor = 'black'
     })
 }
+}
 
+
+//change grid size
+
+const goButton = document.querySelector('#goBtn')
+const gridSize = document.querySelector('#gridSize')
+
+goButton.addEventListener('click', function() {
+    if (num)
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild)
+        
+    }
+
+    createGrid(Number(gridSize.value))
+    hoverEffect()
+})
+
+
+window.onload = createGrid(16); hoverEffect()
